b8d7d459c04e047519ecdb8c1147d0deb77aa065
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-01-31 13:51:02 +0100

    Add tests

---
+++ test/parse.js
@@ -0,0 +1,44 @@
+var DKIMSignature = require( '..' )
+var assert = require( 'assert' )
+
+suite( 'DKIMSignature', function() {
+  
+  suite( '.parse()', function() {
+    
+    test( 'Google Mail', function() {
+      
+      var header = `v=1; a=rsa-sha256; c=relaxed/relaxed;
+       d=gmail.com; s=20120113;
+       h=mime-version:date:message-id:subject:from:to:content-type;
+       bh=DrlXO8ocnosZnW5ZN7P4S/fIdR8vwHj0TyzoPISZF2Q=;
+       b=gOHBExs2JcJFRrozPDw88Js0dc0AHOo6YTZqrDTedfcK/jM/mxfu5rfVzuUnKAGiS5
+       ZvRvXvwYjIW0B9t0DDHDOs5soIukuEXeUw9OV2QD8qc5pmOShuRQWyW5pRftTF87omkj
+       gV2Eik5K2f8FpNlyvuLDjMUmyP8RpLaRrii6+kRRsoJzzP41IqALmlLmJfvtnkeu5kM0
+       v4XnQ4hBNcaLuCmq3fZfCQFDexofECQOZ8FWE0VfdASG8HOJ6jgxuKwYtNfy11ySUSrI
+       wFFlrjTfiNqSD9nzQns3j+xXLtqsvviJQXJgkC8O6mLel3GDwm8LHzBoszzqZ/FiL4rg
+       Vdfw==`
+      
+      var signature = DKIMSignature.parse( header )
+      
+      assert.ok( signature )
+      assert.equal( signature.algorithm, 'rsa-sha256' )
+      assert.equal( signature.canonical, 'relaxed/relaxed' )
+      assert.equal( signature.domain, 'gmail.com' )
+      assert.equal( signature.query, 'dns/txt' )
+      assert.equal( signature.selector, '20120113' )
+      assert.equal( signature.version, '1' )
+      assert.deepEqual( signature.headers, [
+        'mime-version',
+        'date',
+        'message-id',
+        'subject',
+        'from',
+        'to',
+        'content-type'
+      ])
+      
+    })
+    
+  })
+  
+})
