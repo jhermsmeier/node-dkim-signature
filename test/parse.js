64c35e758730a340f8cb69d1b85e89cbf8e6872a
Andy Cork <andy.cork@gmail.com>
2024-04-26 14:35:54 +0100

    Ignore unknown values on signature string

--- test/parse.js
+++ test/parse.js
@@ -38,6 +38,74 @@ suite( 'DKIMSignature', function() {
       ])
       
     })
+
+	test( 'Google Mail with "dara"', function() {
+      
+		var header = `v=1; a=rsa-sha256; c=relaxed/relaxed;
+		 d=gmail.com; s=20120113; dara=google.com; 
+		 h=mime-version:date:message-id:subject:from:to:content-type;
+		 bh=DrlXO8ocnosZnW5ZN7P4S/fIdR8vwHj0TyzoPISZF2Q=;
+		 b=gOHBExs2JcJFRrozPDw88Js0dc0AHOo6YTZqrDTedfcK/jM/mxfu5rfVzuUnKAGiS5
+		 ZvRvXvwYjIW0B9t0DDHDOs5soIukuEXeUw9OV2QD8qc5pmOShuRQWyW5pRftTF87omkj
+		 gV2Eik5K2f8FpNlyvuLDjMUmyP8RpLaRrii6+kRRsoJzzP41IqALmlLmJfvtnkeu5kM0
+		 v4XnQ4hBNcaLuCmq3fZfCQFDexofECQOZ8FWE0VfdASG8HOJ6jgxuKwYtNfy11ySUSrI
+		 wFFlrjTfiNqSD9nzQns3j+xXLtqsvviJQXJgkC8O6mLel3GDwm8LHzBoszzqZ/FiL4rg
+		 Vdfw==`
+		
+		var signature = DKIMSignature.parse( header )
+		
+		assert.ok( signature )
+		assert.equal( signature.algorithm, 'rsa-sha256' )
+		assert.equal( signature.canonical, 'relaxed/relaxed' )
+		assert.equal( signature.domain, 'gmail.com' )
+		assert.equal( signature.query, 'dns/txt' )
+		assert.equal( signature.selector, '20120113' )
+		assert.equal( signature.version, '1' )
+		assert.deepEqual( signature.headers, [
+		  'mime-version',
+		  'date',
+		  'message-id',
+		  'subject',
+		  'from',
+		  'to',
+		  'content-type'
+		])
+		
+	})
+
+	test( 'Google Mail with "darn"', function() {
+      
+		var header = `v=1; a=rsa-sha256; c=relaxed/relaxed;
+		 d=gmail.com; s=20120113; darn=google.com; 
+		 h=mime-version:date:message-id:subject:from:to:content-type;
+		 bh=DrlXO8ocnosZnW5ZN7P4S/fIdR8vwHj0TyzoPISZF2Q=;
+		 b=gOHBExs2JcJFRrozPDw88Js0dc0AHOo6YTZqrDTedfcK/jM/mxfu5rfVzuUnKAGiS5
+		 ZvRvXvwYjIW0B9t0DDHDOs5soIukuEXeUw9OV2QD8qc5pmOShuRQWyW5pRftTF87omkj
+		 gV2Eik5K2f8FpNlyvuLDjMUmyP8RpLaRrii6+kRRsoJzzP41IqALmlLmJfvtnkeu5kM0
+		 v4XnQ4hBNcaLuCmq3fZfCQFDexofECQOZ8FWE0VfdASG8HOJ6jgxuKwYtNfy11ySUSrI
+		 wFFlrjTfiNqSD9nzQns3j+xXLtqsvviJQXJgkC8O6mLel3GDwm8LHzBoszzqZ/FiL4rg
+		 Vdfw==`
+		
+		var signature = DKIMSignature.parse( header )
+		
+		assert.ok( signature )
+		assert.equal( signature.algorithm, 'rsa-sha256' )
+		assert.equal( signature.canonical, 'relaxed/relaxed' )
+		assert.equal( signature.domain, 'gmail.com' )
+		assert.equal( signature.query, 'dns/txt' )
+		assert.equal( signature.selector, '20120113' )
+		assert.equal( signature.version, '1' )
+		assert.deepEqual( signature.headers, [
+		  'mime-version',
+		  'date',
+		  'message-id',
+		  'subject',
+		  'from',
+		  'to',
+		  'content-type'
+		])
+		
+	  })
     
     test( 'Mandrill Mail With Spaces', function() {
       
